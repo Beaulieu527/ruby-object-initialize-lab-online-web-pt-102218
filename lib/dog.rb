696f072ca73cc02bec24f951fb0fe7e757147535
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-08 00:40:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,21 @@ class Dog
 
   def initialize(name,breed)
     @name = name
-
-    def breed(breed)
-      if @breed = breed
-        puts breed
-      else
-        puts "Mutt"
-      end
-    end
+    @breed = breed
   end
 
   attr_accessor :name
-  attr_accessor :breed
+
+  def breed=(breed)
+    if @breed = breed
+      puts breed
+    else
+      puts "Mutt"
+    end
+  end
+
+  def breed
+    @breed
+  end
 
 end
